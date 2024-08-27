a2c77a7ebfc7e746888cf9e30d7df1aa5e47827d
eripratama1 <eripratama90@gmail.com>
2024-08-27 21:08:12 +0800

    add darkmode function

---
+++ public/assets/js/darkmode.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    /**
+     * Di bagian kode ini ketika halaman pertama kali dimuat
+     * akan melakukan pengecekan key "theme" yang tersimpan pada localStorage
+     * jika nilai "theme" yang tersimpan bernilai dark tampilkan warna tema menjadi gelap
+     * dan ubah button dan text-nya menjadi Light Mode
+     *
+     * Begitu juga sebaliknya ketika nilai "theme" bernilai light ubah tema menjadi terang
+     * dan ubah button dan text-nya menjadi Dark Mode
+     */
+    const theme = localStorage.getItem('theme');
+    const btnText = document.getElementById('btnText');
+    const iconPath = document.getElementById('iconPath');
+
+    if (theme) {
+        document.documentElement.setAttribute('data-bs-theme', theme);
+
+        if (theme === 'dark') {
+            btnText.textContent = 'Light Mode';
+            iconPath.setAttribute('d',
+            'M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708')
+        } else {
+            btnText.textContent = 'Dark Mode';
+            iconPath.setAttribute('d',
+            'M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278M4.858 1.311A7.27 7.27 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.32 7.32 0 0 0 5.205-2.162q-.506.063-1.029.063c-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286')
+        }
+    }
+
+    /**
+     * Ketika button dengan id btnSwitch di klik maka update warna tema
+     * dan simpan nilainya pada localStorage. Dan juga update button text dan icon berdasarkan
+     * nilai yang ada di localStorage
+     */
+    document.getElementById('btnSwitch').addEventListener('click', () => {
+        const theme = document.documentElement.getAttribute('data-bs-theme');
+        const btnText = document.getElementById('btnText');
+        const iconPath = document.getElementById('iconPath');
+
+        if (theme) {
+            document.documentElement.setAttribute('data-bs-theme', theme);
+
+            if (theme === 'dark') {
+                document.documentElement.setAttribute('data-bs-theme','light')
+                btnText.textContent = 'Dark Mode';
+                iconPath.setAttribute('d', 'M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278M4.858 1.311A7.27 7.27 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.32 7.32 0 0 0 5.205-2.162q-.506.063-1.029.063c-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286')
+                localStorage.setItem('theme','light')
+            } else {
+                document.documentElement.setAttribute('data-bs-theme','dark')
+                btnText.textContent = 'Light Mode';
+                iconPath.setAttribute('d', 'M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708')
+                localStorage.setItem('theme','dark')
+            }
+        }
+
+    })
+
+})
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-bs-theme="light">
 
 <head>
     <meta charset="utf-8">
@@ -21,7 +21,7 @@
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -35,8 +35,18 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
+
+                        <button class="btn btn-outline" id="btnSwitch">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-moon" viewBox="0 0 16 16">
+                                <path id="iconPath"
+                                    d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278M4.858 1.311A7.27 7.27 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.32 7.32 0 0 0 5.205-2.162q-.506.063-1.029.063c-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286" />
+                            </svg>
+                            <span id="btnText">Dark Mode</span>
+                        </button>
+
                         <li class="nav-item">
-                            <a class="nav-link" href="{{route('categories')}}">Kategori</a>
+                            <a class="nav-link" href="{{ route('categories') }}">Kategori</a>
                         </li>
                     </ul>
 
@@ -57,11 +67,11 @@
                             @endif
                         @else
                             <li class="nav-item">
-                                <a class="nav-link" href="{{route('category.index')}}">Category</a>
+                                <a class="nav-link" href="{{ route('category.index') }}">Category</a>
                             </li>
 
                             <li class="nav-item">
-                                <a class="nav-link" href="{{route('spot.index')}}">Spot</a>
+                                <a class="nav-link" href="{{ route('spot.index') }}">Spot</a>
                             </li>
 
                             <li class="nav-item dropdown">
@@ -92,6 +102,7 @@
             @yield('content')
             @stack('javascript')
         </main>
+        <script src="{{asset('assets/js/darkmode.js')}}"></script>
     </div>
 </body>
 
